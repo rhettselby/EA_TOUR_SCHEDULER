2edb8a2245252d5a99e4303633a7c6e7af719ee7
Rhett Selby <rhettselby10@gmail.com>
2026-03-02 13:24:56 -0800

    removed --single-process argument

--- tours/tasks.py
+++ tours/tasks.py
@@ -61,7 +61,6 @@ def TourScraper():
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--single-process")
         options.binary_location = "/usr/bin/google-chrome"
 
         driver = webdriver.Chrome(
