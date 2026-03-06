0f31607961ced0044b127be98244f1757896ff43
Rhett Selby <rhettselby10@gmail.com>
2026-03-05 20:35:21 -0800

    fixed type in tours update_status url pattern preventing tour status refresh issue

--- tours/urls.py
+++ tours/urls.py
@@ -6,7 +6,7 @@ app_name = 'tours'
 urlpatterns = [
     path('', views.tours_api),
     path('get_tours/', views.get_tours, name = "get_tours"),
-    path('<int>:tour_id/update_status/', views.update_status, name = "update_status"),
+    path('<int:tour_id>/update_status/', views.update_status, name = "update_status"),
 ]
 
 
