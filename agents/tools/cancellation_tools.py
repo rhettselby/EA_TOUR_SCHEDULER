73f970b15c5e9cc70d7ddedcf73c027d56d6c0d7
Rhett Selby <rhettselby10@gmail.com>
2026-04-09 14:32:09 -0700

    Updated cancellation agent message

--- agents/tools/cancellation_tools.py
+++ agents/tools/cancellation_tools.py
@@ -14,7 +14,7 @@ slack_client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 def slack_cancellation(time:str, guest_name:str, week_number:int):
     try:
 
-        text = f"@channel (no ping for testing) Cancelled Tour at {time} (week {week_number}) for {guest_name}"
+        text = f"@channel (no ping for testing) {guest_name} has cancelled their tour at {time} (Week {week_number}). Please notify the ambassador assigned to this tour."
         slack_client.chat_postMessage(
             channel = CANCELLATION_CHANNEL_ID,
             text = text,
