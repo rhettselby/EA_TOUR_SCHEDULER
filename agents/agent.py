34799ea77ceee89d2015d70a600274175027dbc1
Rhett Selby <rhettselby10@gmail.com>
2026-04-25 22:25:30 -0700

    fixed root agent bug

--- agents/agent.py
+++ agents/agent.py
@@ -45,30 +45,20 @@ root_agent_fallback = Agent(
     Your job is to delegate work to the sub-agent that would best handle
     the given work. Here is a list of the available sub-agents.
 
-    1. slack_agent:
+    1. slack_agent_fallback:
         The slack agent is responsible for sending messages in the engineering ambassador slack channel in order
         to coordinate tour assignment within the club. This includes sending an initial message to the appropriate channel 
         when a new tour is recieved, and following up on that message, handling any questions it receives.
 
-    2. cancellation_agent:
+    2. cancellation_agent_fallback:
         The cancellation_agent is responsible for tour cancellations. It's job is to send slack notifications regarding a 
         specific tour cancellation to the correct slack channel.
 
-    3. slack_agent_fallback:
-        The slack_agent_fallback is responsilbe for the slack agent's tasks when the slack agent fails to complete its tasks.
-    
-    4. cancellation_agent_fallback:
-        The cancellation_agent_fallback is responsible for the cancellation agent's tasks when the cancellation agent fails to complete its tasks
-
     It is extremely important that you select the sub_agent that aligns most accurately with the given task.
-
-    Additionally, if the intitial agent you delegate to fails to complete their task, for instance if the model is experiencing high demand
-    and rejects the request, then call the appropriate fallback model for that sub agent. 
-    slack_agent's fallback is slack_agent_fallback and cancellation_agent's fallback is cancellation_agent_fallback
     """,
 
     tools=[],
-    sub_agents=[slack_agent, cancellation_agent, slack_agent_fallback, cancellation_agent_fallback]
+    sub_agents=[slack_agent_fallback, cancellation_agent_fallback]
 )
 
 
