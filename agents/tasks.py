6a4837a580f52087607b13d8d237328ebce25994
Rhett Selby <rhettselby10@gmail.com>
2026-05-18 13:03:06 -0700

    add try block to run_slack_agent task

--- agents/tasks.py
+++ agents/tasks.py
@@ -6,5 +6,9 @@ from tours.models import Tour
 
 @shared_task
 def run_slack_agent(channel, text, time_stamp):
-    query = f"A message was sent in slack at {time_stamp}. Please respond to the following message in channel {channel}: {text}"
-    asyncio.run(run_agent(query, channel))
+    try:
+        print("sending slack_agent message")
+        query = f"A message was sent in slack at {time_stamp}. Please respond to the following message in channel {channel}: {text}"
+        asyncio.run(run_agent(query, channel))
+    except Exception as e:
+        print(str(e))
