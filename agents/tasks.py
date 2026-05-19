517ca0b55b02a05f4d3d963828bb91dded253df2
Rhett Selby <rhettselby10@gmail.com>
2026-05-18 18:30:19 -0700

    modifying initial general reply query to improve clarity

--- agents/tasks.py
+++ agents/tasks.py
@@ -9,7 +9,7 @@ from tours.models import Tour
 def run_slack_agent(channel, text, time_stamp):
     try:
         print("sending slack_agent message")
-        query = f"A message was sent in slack at {time_stamp}. Please respond to the following message in channel {channel}: {text}"
+        query = f"GENERAL SLACK REPLY TASK: A message was sent in slack at {time_stamp}. Please respond to the following message in channel {channel}: {text}"
         asyncio.run(run_agent(query, channel))
     except Exception as e:
         print(str(e))
