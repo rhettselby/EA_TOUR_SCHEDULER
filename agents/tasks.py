a3694dfe078750055888a6d87f0e741d53091711
Rhett Selby <rhettselby10@gmail.com>
2026-05-29 10:59:34 -0700

    update slack bot query to only reply when directly named

--- agents/tasks.py
+++ agents/tasks.py
@@ -10,7 +10,7 @@ def run_slack_agent(channel, text, time_stamp):
     try:
         print("sending slack_agent message")
         query = f"""GENERAL SLACK REPLY TASK: A message was sent in slack at {time_stamp}. Please respond to the following message in channel {channel}: {text}
-            Only respond to the message if it is relating to tour coordination/scheduling, the message directly references the slack/tour/bot agent, or the message is part of a longer conversation
+            Only respond to the message if the message directly references the slack/tour/bot agent, or the message is part of a longer conversation
             with the slack agent (you). 
             """
         asyncio.run(run_agent(query, channel))
