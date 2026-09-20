9ee30d872f306f8a45e6b1115aff624f88734e64
Rhett Selby <rhettselby10@gmail.com>
2026-09-20 09:23:23 -0700

    Fall 26 slack channel ID's

--- agents/tools/cancellation_tools.py
+++ agents/tools/cancellation_tools.py
@@ -9,63 +9,6 @@ import pytz
 CANCELLATION_CHANNEL_ID = "C0AKSD2DQ06"
 
 
-CHANNEL_MAP = {
-    "Monday_9": "C0AM295E423",
-    "Monday_10": "C0ANC0ZFZFA",
-    "Monday_11": "C0AM29D94AK",
-    #
-    "Monday_12": "C0B4S8BTVT5",
-    "Monday_13": "C0AMEA95ABF",
-    ##
-    "Monday_14": "C0B50DNJKEV",
-    ##
-    "Monday_15": "C0AMMAP03M2",
-    "Monday_16": "C0B4S8BTVT5",
-    "Tuesday_9": "C0AMHP0F0SW",
-    "Tuesday_10": "C0AMFN16BF0",
-    "Tuesday_11": "C0AM29XE6G7",
-    #
-    "Tuesday_12": "C0B4S8BTVT5",
-    "Tuesday_13": "C0AMHPARKBL",
-    ##
-    "Tuesday_14": "C0B50DNJKEV",
-    ##
-    "Tuesday_15": "C0AM2A7RK2B",
-    "Tuesday_16": "C0B4S8BTVT5",
-    "Wednesday_9": "C0AMVQUSLQZ",
-    "Wednesday_10": "C0AMGTR96E6",
-    "Wednesday_11": "C0AMAGCQSCT",
-    #
-    "Wednesday_12": "C0B4S8BTVT5",
-    "Wednesday_13": "C0AMGTZK6UA",
-    ##
-    "Wednesday_14": "C0B50DNJKEV",
-    ##
-    "Wednesday_15": "C0AMDFRC8PP",
-    "Wednesday_16": "C0B4S8BTVT5",
-    "Thursday_9": "C0AMAGWRQAF",
-    "Thursday_10": "C0ANB7394KS",
-    "Thursday_11": "C0AM1FD6N23",
-    #
-    "Thursday_12": "C0B4S8BTVT5",
-    "Thursday_13": "C0AMGURQS3C",
-    ##
-    "Thursday_14": "C0B50DNJKEV",
-    ##
-    "Thursday_15": "C0AMVS6N29F",
-    "Thursday_16": "C0B4S8BTVT5",
-    "Friday_9": "C0ANC2XNCQG",
-    "Friday_10": "C0AM2B5D3F1",
-    "Friday_11": "C0ANC31RSN4",
-    #
-    "Friday_12": "C0B4S8BTVT5",
-    "Friday_13": "C0AM2BJ5F3R",
-    ##
-    "Friday_14": "C0B50DNJKEV",
-    ##
-    "Friday_15": "C0ANC3G4H4G",
-    "Friday_16": "C0B4S8BTVT5",
-}
 
 
 def get_channel_id(week_day:str, time: int) -> dict:
@@ -76,9 +19,8 @@ def get_channel_id(week_day:str, time: int) -> dict:
     print(f"getting channel _id")
 
     try:
+        #call function from slack tools
         channel_id = get_channel_id(week_day, time)
-        #key = week_day + "_" + str(time)
-        #channel_id = CHANNEL_MAP[key]
         return {
             "channel_id": channel_id,
             "status": "retrieved channel id",
@@ -87,7 +29,7 @@ def get_channel_id(week_day:str, time: int) -> dict:
     except Exception as e:
        return {
            "channel_id": "C0AKSD2DQ06",
-            "status": "Unable to retrive channel id",
+            "status": f"Unable to retrive channel id, error : {str(e)}",
             }
 
 slack_client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
